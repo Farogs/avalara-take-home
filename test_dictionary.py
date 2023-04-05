d1b6d96da252ab87c23848f8d97cbd17629711a1
Umaru <faarooq.arogundade@gmail.com>
2023-04-05 12:24:27 -0700

    Update code file namr in test file

--- test_dictionary.py
+++ test_dictionary.py
@@ -1,22 +1,22 @@
 import unittest
 from unittest.mock import patch
-from dictioncli import get_word_definition
+from dictionarycli import get_word_definition
 
 class TestDictionary(unittest.TestCase):
-    @patch('dictioncli.requests.get')
+    @patch('dictionarycli.requests.get')
     def test_get_word_definition(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = [{'shortdef': ['a small, furry, nocturnal mammal'], 'fl': 'noun'}]
         response = get_word_definition('squirrel')
         self.assertEqual(response, 'Squirrel (noun): a small, furry, nocturnal mammal')
 
-    @patch('dictioncli.requests.get')
+    @patch('dictionarycli.requests.get')
     def test_get_word_definition_not_found(self, mock_get):
         mock_get.return_value.status_code = 404
         response = get_word_definition('squirrel')
         self.assertEqual(response, 'Word not found')
 
-    @patch('dictioncli.requests.get')
+    @patch('dictionarycli.requests.get')
     def test_get_word_definition_no_definition(self, mock_get):
         mock_get.return_value.status_code = 200
         mock_get.return_value.json.return_value = [{'shortdef': []}]
