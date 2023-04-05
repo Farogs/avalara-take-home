a2939f38d4940c134d3c26562dab296f46298d87
Umaru <faarooq.arogundade@gmail.com>
2023-04-05 12:17:26 -0700

    Add unit test for word not found

--- test_dictionary.py
+++ test_dictionary.py
@@ -9,3 +9,9 @@ class TestDictionary(unittest.TestCase):
         mock_get.return_value.json.return_value = [{'shortdef': ['a small, furry, nocturnal mammal'], 'fl': 'noun'}]
         response = get_word_definition('squirrel')
         self.assertEqual(response, 'Squirrel (noun): a small, furry, nocturnal mammal')
+
+    @patch('dictioncli.requests.get')
+    def test_get_word_definition_not_found(self, mock_get):
+        mock_get.return_value.status_code = 404
+        response = get_word_definition('squirrel')
+        self.assertEqual(response, 'Word not found')
